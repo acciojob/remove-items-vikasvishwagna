138c99300f19d115ccd29f6d682d4a87307c0bec
k.v.sai vikas <130644581+vikasvishwagna@users.noreply.github.com>
2025-06-26 20:45:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var deleteBtn = document.queryselector("button");
+
+
+deleteBtn.addEventListner("click",function () {
+	var colorSelect = document.queryselector("colorSelect");
+	var selectIdx = colorSelect.selectIndex;
+
+	if(selectIdx != -1){
+		colorSelect.remove(selectIdx);
+	}
+})
